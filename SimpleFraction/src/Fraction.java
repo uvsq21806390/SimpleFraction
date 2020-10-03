9211a5e135576e92adec08c965254633b10bf3a4
uvsq21806390 <melissa.houamel@ens.uvsq.fr>
2020-10-03 23:34:52 +0200

    SimpleFraction project -TD1-

---
+++ SimpleFraction/src/Fraction.java
@@ -0,0 +1,9 @@
+/**
+ * Fraction class
+ * @author Melissa HOUAMEL 21806390
+ * @version 02.10.20
+ *
+ */
+public class Fraction {
+
+}
---
+++ SimpleFraction/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Main program
+ * @author Melissa HOUAMEL 21806390
+ * @version 02.10.20
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD!!!");
+
+	}
+
+}
