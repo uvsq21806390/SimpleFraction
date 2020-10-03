ffbd0679f17aabdb2119b0500c42e4f13384b023
uvsq21806390 <melissa.houamel@ens.uvsq.fr>
2020-10-03 23:50:19 +0200

    Simple Fraction Project

--- SimpleFraction/src/Fraction.java
+++ SimpleFraction/src/Fraction.java
@@ -5,5 +5,33 @@
  *
  */
 public class Fraction {
+	
+	private int x;
+	private int y;
+	private double value;
+	
+	public Fraction(int x,int y) {
+		this.x= x;
+		this.y= y;
+		this.value= (double) x/y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public double getValue() {
+		return this.value;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return this.x+" / "+this.y+" = "+ this.value;
+	}
 
 }
--- SimpleFraction/src/Main.java
+++ SimpleFraction/src/Main.java
@@ -7,7 +7,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD!!!");
+		
+		System.out.println("toString Fraction test:");
+		Fraction f1 =new Fraction(7,13);
+		System.out.println(f1.toString());
 
 	}
 
